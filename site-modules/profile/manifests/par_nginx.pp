add431409824aa753265f96685b8ec3297684346
Garrett Rowell <garrett@puppet.com>
2025-10-23 14:30:00 -0600

    test vars

--- site-modules/profile/manifests/par_nginx.pp
+++ site-modules/profile/manifests/par_nginx.pp
@@ -1,6 +1,9 @@
 class profile::par_nginx {
   par { 'deploy-nginx':
-    ensure   => present,
-    playbook => '/root/.ansible/collections/ansible_collections/nginxinc/nginx_core/playbooks/deploy-nginx.yml',
+    ensure        => present,
+    playbook      => '/root/.ansible/collections/ansible_collections/nginxinc/nginx_core/playbooks/deploy-nginx.yml',
+    playbook_vars => {
+      nginx_start => false,
+    },
   }
 }
