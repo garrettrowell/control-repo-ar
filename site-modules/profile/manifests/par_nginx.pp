2fed8b7299f2b6123588cad6a065dc00b27a5816
Garrett Rowell <garrett@puppet.com>
2025-10-29 12:53:20 -0600

    hack

--- site-modules/profile/manifests/par_nginx.pp
+++ site-modules/profile/manifests/par_nginx.pp
@@ -2,6 +2,9 @@ class profile::par_nginx {
   class { 'paw_nginxinc_nginx_core::nginx':
     nginx_start => false,
   }
+  Par <| title == 'paw_nginxinc_nginx_core::nginx-main'|> {
+    playbook => "${facts['puppet_vardir']}/lib/puppet_x/ansible_modules/paw_nginxinc_nginx_core/roles/nginx/playbook.yml"
+  }
   #par { 'deploy-nginx':
   #  playbook      => '/root/.ansible/collections/ansible_collections/nginxinc/nginx_core/playbooks/deploy-nginx.yml',
   #  playbook_vars => {
