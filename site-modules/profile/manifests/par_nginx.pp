989a2040bcd55ad3273952faad54acbdbe9d0001
Garrett Rowell <garrett@puppet.com>
2025-10-29 12:55:54 -0600

    more

--- site-modules/profile/manifests/par_nginx.pp
+++ site-modules/profile/manifests/par_nginx.pp
@@ -3,7 +3,7 @@ class profile::par_nginx {
     nginx_start => false,
   }
   Par <| title == 'paw_nginxinc_nginx_core::nginx-main'|> {
-    playbook => "${facts['puppet_vardir']}/lib/puppet_x/ansible_modules/paw_nginxinc_nginx_core/roles/nginx/playbook.yml"
+    playbook => "${facts['puppet_vardir']}/lib/puppet_x/ansible_modules/nginxinc_nginx_core/roles/nginx/playbook.yml"
   }
   #par { 'deploy-nginx':
   #  playbook      => '/root/.ansible/collections/ansible_collections/nginxinc/nginx_core/playbooks/deploy-nginx.yml',
