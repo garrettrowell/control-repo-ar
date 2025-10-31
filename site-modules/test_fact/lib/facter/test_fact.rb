098ab011a11e5968a7c4a0c52ffb38d1625b1688
Garrett Rowell <garrett@puppet.com>
2025-10-31 09:25:46 -0600

    more

--- site-modules/test_fact/lib/facter/test_fact.rb
+++ site-modules/test_fact/lib/facter/test_fact.rb
@@ -6,6 +6,11 @@ Facter.add(:test_fact) do
     key3 = Facter::Core::Execution.execute('/usr/bin/idontexist', on_fail: nil)
     test_fact['key3'] = key3 unless key3.nil? || key3.empty?
     test_fact['key4'] = Facter::Core::Execution.execute('/usr/bin/idontexist', on_fail: '')
+    test_fact['key5'] = {}
+    test_fact['key6'] = {}
+    nest1 = Facter::Core::Execution.execute('/usr/bin/idontexist -r', on_fail: nil)
+    test_fact['key6']['nest1'] = nest1 unless nest1.nil? || nest1.empty?
+    test_fact['key6']['nest2'] = 'value6nest2'
     test_fact
   end
 end
